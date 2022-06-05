66ae841a34501b6b08bf013c302e5e4d1582f54a
George Sima <bogdansima20@gmail.com>
2022-06-06 00:48:45 +0100

    initial design

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import React from 'react';
-import './ToastMain.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import ToastMain from '../ToastMain';
+import Navbar from '../Navbar';
+
+// Query client for react-query
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <div className="App">
-            Welcome to React
-        </div>
+        <>
+            <Router>
+                <div className='App w-screen h-screen'>
+                    <QueryClientProvider client={queryClient}>
+                        {/* <Navbar /> */}
+                        <Routes>
+                            <Route path="/" element={<ToastMain />} />
+                        </Routes>
+                    </QueryClientProvider>
+                </div>
+            </Router>
+        </>
     );
 }
 
